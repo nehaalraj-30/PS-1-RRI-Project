d69041315f2fd67ae8f6d27528828d6fdc6cbdd5
nehaalraj-30 <145157148+nehaalraj-30@users.noreply.github.com>
2025-05-30 11:20:51 +0530

    Add files via upload

---
+++ adc.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def sine_curve(f,sampling_rate,duration,v_ref):
+    n_samples = int(duration*sampling_rate)
+    t = np.linspace(0,duration,n_samples, endpoint= False)
+    noise = 0*np.random.normal(0,0.09,t.shape)
+    scaling_factor = 1
+    sine_sig = noise + scaling_factor*(np.sin(2*np.pi*(f)*t.astype(np.float64))+1)*(v_ref/2)
+    # sine_sig = np.full_like(t,1.5)
+    
+    return t,sine_sig
+
+def sample(time,adc_sampling_rate,signal):
+    step = int(len(time) / (adc_sampling_rate*(time[-1]-time[0]))) 
+    return time[::step],signal[::step]
+
+def adc(vin, n_bits, v_ref):
+    vin_clipped = np.clip(vin,0,v_ref)
+    return int(np.floor((vin_clipped/v_ref)*(2**n_bits-1)))
+
+n_bits = 12
+v_ref = 3.3
+f = 2e8
+sampling_rate = 1e12 #in hz not our adc sample
+adc_sampling_rate = 4e9
+time_period = 10
+duration = time_period*(1/f)
+time,vin_values = sine_curve(f,sampling_rate,duration,v_ref)
+adc_time,adc_signal = sample(time,adc_sampling_rate,vin_values)
+digital_values = [adc(vin,n_bits,v_ref) for vin in adc_signal]
+
+plt.plot(adc_time*1e9, adc_signal,linestyle = 'dashed')
+plt.xlabel('t(ns)')
+plt.ylabel('amplitude')
+plt.title('signal_sampled')
+plt.grid(True)
+plt.savefig("input_adc_sampled.png")
+
+plt.plot(time*1e9, vin_values)
+plt.xlabel('t(ns)')
+plt.ylabel('amplitude')
+plt.title('signal')
+plt.grid(True)
+plt.savefig("input_adc.png")
+
+
+    
+plt.step(adc_time*1e9, digital_values, where='post')
+plt.xlabel('time')
+plt.ylabel('Digital Output (Decimal)')
+plt.title(f'{n_bits}-bit ADC Conversion')
+plt.grid(True)
+# plt.xlim(0,1)
+plt.savefig("adc.png")
+
+
+
+    
